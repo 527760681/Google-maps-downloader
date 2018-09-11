f64a54afc044d8ac0526cefcfbdaba0b348b5d47
wangjun <527760681@qq.com>
2018-09-11 17:55:05 +0800

    new files

---
+++ Google maps downloader/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Google_maps_downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.address_Textbox = new System.Windows.Forms.TextBox();
+            this.go_Button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // address_Textbox
+            // 
+            this.address_Textbox.Location = new System.Drawing.Point(12, 12);
+            this.address_Textbox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.address_Textbox.Name = "address_Textbox";
+            this.address_Textbox.Size = new System.Drawing.Size(984, 21);
+            this.address_Textbox.TabIndex = 1;
+            this.address_Textbox.Text = "http://www.google.cn/maps/@40.0729145,116.5927644,584m/data=!3m1!1e3";
+            //this.address_Textbox.Text = "https://www.google.com/maps/@40.073,116.60808,220m/data=!3m1!1e3";
+            // 
+            // go_Button
+            // 
+            this.go_Button.Location = new System.Drawing.Point(1000, 11);
+            this.go_Button.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.go_Button.Name = "go_Button";
+            this.go_Button.Size = new System.Drawing.Size(82, 23);
+            this.go_Button.TabIndex = 2;
+            this.go_Button.Text = "访问";
+            this.go_Button.UseVisualStyleBackColor = true;
+            this.go_Button.Click += new System.EventHandler(this.go_Button_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1086, 351);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "截图";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1070, 612);
+            this.webBrowser1.TabIndex = 4;
+            this.webBrowser1.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.webBrowser1_Navigated);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(12, 38);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1070, 612);
+            this.panel1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Google_maps_downloader.Properties.Resources._1;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(1086, 394);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Adobe Gothic Std B", 8F, System.Drawing.FontStyle.Bold);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 14;
+            this.listBox1.Items.AddRange(new object[] {
+            "agricultural",
+            "airplane",
+            "baseballdiamond",
+            "beach",
+            "buildings",
+            "chaparral",
+            "denseresidential",
+            "forest",
+            "freeway",
+            "golfcourse",
+            "harbor",
+            "intersection",
+            "mediumresidential",
+            "mobilehomepark",
+            "overpass",
+            "parkinglot",
+            "river",
+            "runway",
+            "sparseresidential",
+            "storagetanks",
+            "tenniscourt"});
+            this.listBox1.Location = new System.Drawing.Point(1086, 38);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(256, 298);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1352, 662);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.go_Button);
+            this.Controls.Add(this.address_Textbox);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "谷歌图像截取";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+        private System.Windows.Forms.TextBox address_Textbox;
+        private System.Windows.Forms.Button go_Button;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Google maps downloader/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using ReadWriteCsv;
+namespace Google_maps_downloader
+{
+    public partial class Form1 : Form
+    {
+        Bitmap img;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+
+        }
+
+        private void go_Button_Click(object sender, EventArgs e)
+        {
+            Navigate(address_Textbox.Text);
+        }
+
+        private void Navigate(String address)
+        {
+            if (String.IsNullOrEmpty(address)) return;
+            if (address.Equals("about:blank")) return;
+            if (!address.StartsWith("http://") && !address.StartsWith("https://"))
+            {
+                address = "http://" + address;
+            }
+            try
+            {
+                webBrowser1.Navigate(address);
+            }
+            catch (System.UriFormatException)
+            {
+                return;
+            }
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            this.webBrowser1.Document.MouseMove += new HtmlElementEventHandler(this.document_OnMouseMove);
+            MessageBox.Show("页面加载完成");
+        }
+
+        private void webBrowser1_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            address_Textbox.Text = webBrowser1.Url.ToString();
+
+        }
+
+        public string parseLonlat(string googleUrl)
+        {
+            string lonlat = "";
+
+            string pattern = @"@-?\d+.\d+,-?\d+.\d+";
+
+            foreach (Match match in Regex.Matches(googleUrl, pattern))
+                lonlat = match.Value;
+
+            return lonlat;
+        }
+
+        public bool saveFile(string lonlat, string classes, Bitmap img)
+        {
+            try
+            {
+                if (!Directory.Exists(classes))
+                {
+                    Directory.CreateDirectory(classes);
+                }
+
+                string guid = Guid.NewGuid().ToString().Replace("-", "");
+                string imgFilename = classes + @"\" + guid + ".tif";
+                img.Save(imgFilename);
+                using (CsvFileWriter writer = new CsvFileWriter("classes.csv",true))
+                {
+                    CsvRow csvRow = new CsvRow();
+                    csvRow.Add(lonlat);
+                    csvRow.Add(classes);
+                    csvRow.Add(imgFilename);
+
+                    writer.WriteRow(csvRow);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+            
+
+        }
+
+        private void document_OnMouseMove(object sender, HtmlElementEventArgs e)
+        {
+            img = new Bitmap(256, 256);
+            Point sP = this.webBrowser1.PointToScreen(this.webBrowser1.Location);
+            sP.X += 435;
+            sP.Y += 200;
+            Graphics g = Graphics.FromImage(img);
+
+            g.CopyFromScreen(sP, Point.Empty, this.webBrowser1.ClientSize);
+
+            pictureBox1.Image = img;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Hide();
+                string classes = this.listBox1.SelectedItem.ToString();
+                string lonlat = parseLonlat(webBrowser1.Url.ToString()).Replace("\"", "");
+                bool result = saveFile(lonlat, classes, img);
+                if (result)
+                {
+                    MessageBox.Show("保存成功");
+
+                }
+                else
+                {
+                    MessageBox.Show("保存失败");
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.StackTrace);
+            }
+            finally
+            {
+                this.Show();
+            }
+        }
+
+    }
+}
